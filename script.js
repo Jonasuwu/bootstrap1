c63cacaa9da5b22094fcfb4b3170ce04019df8e2
ThizinhoSRS <108826418+Jonasuwu@users.noreply.github.com>
2022-08-23 17:54:35 -0300

    Cnages
    js iniciado

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+});
+
+$("#btnOlaMundo").click(function(){
+ alert("Ola Mundo Meu Js esta ok  : )");
+
+ $("body").addClass("olamundo");
+
+});
+
+$("#btnfundo").click(function() {
+
+$("body").addClass("fundo");
+
+});
+
+$("#btnesconder").click(function() {
+$("h1").hide();
+});
+
